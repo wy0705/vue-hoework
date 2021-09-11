426834e632b8b81ebeed35430399c43b7b948a50
wy0705 <8617935+wy0705@user.noreply.gitee.com>
2021-09-11 19:05:10 +0800

    33

---
+++ wy/src/components/page5.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>
+      Tableau实战系列Tableau基础概念全解析
+    </h2>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      与电子表格不同，数据库通常对数据类型实施严格的规则。数据类型对给定字段中的数据 进行分类，并提供有关如何格式化、解释数据以及可以对该数据执行哪些操作的信息
+    cd </p>
+  </div>
+</template>
